d1e35727ed3dfbc03be06b206224484f4d2e3558
MrHeer <hlm52pk@163.com>
2020-06-25 10:34:51 +0800

    :art: add eslint and prettier
    :art: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Markdown } from './components'
-import { Button } from 'antd';
-import { DownloadOutlined } from '@ant-design/icons';
+import React from "react";
+import { Markdown } from "./components";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-      <a className='download' href='/resume/resume.pdf' target='_blank'>
-        <Button shape="circle" icon={<DownloadOutlined />}
-          size='large' />
+      <a className="download" href="/resume/resume.pdf" target="_blank">
+        <Button shape="circle" icon={<DownloadOutlined />} size="large" />
       </a>
       <Markdown filePath="/resume/resume.md" />
     </div>
--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -1,32 +1,39 @@
-import React, { useState, useMemo, useEffect } from 'react';
-import marked from 'marked';
-import 'github-markdown-css';
-import './style'
+import React, { useState, useMemo, useEffect } from "react";
+import marked from "marked";
+import "github-markdown-css";
+import "./style";
 
 interface MarkdownProps {
-    filePath: string;
+  filePath: string;
 }
 
 const Markdown: React.SFC<MarkdownProps> = (props) => {
-    const { filePath } = props;
-    const [markdown, setMarkdown] = useState("");
+  const { filePath } = props;
+  const [markdown, setMarkdown] = useState("");
 
-    useEffect(() => {
-        fetch(filePath).then(res => {
-            return res.text();
-        }).then(cxt => {
-            setMarkdown(cxt);
-        }).catch(reason => {
-            console.error(reason);
-        });
+  useEffect(() => {
+    fetch(filePath)
+      .then((res) => {
+        return res.text();
+      })
+      .then((cxt) => {
+        setMarkdown(cxt);
+      })
+      .catch((reason) => {
+        console.error(reason);
+      });
+  }, [filePath]);
 
-    }, [filePath]);
+  const innerMarkHTML = useMemo(() => {
+    return { __html: marked(markdown) };
+  }, [markdown]);
 
-    const innerMarkHTML = useMemo(() => {
-        return { __html: marked(markdown) }
-    }, [markdown])
-
-    return (<article className='markdown-body' dangerouslySetInnerHTML={innerMarkHTML} />);
-}
+  return (
+    <article
+      className="markdown-body"
+      dangerouslySetInnerHTML={innerMarkHTML}
+    />
+  );
+};
 
 export default Markdown;
