8589ecf854d763523340660ed45f834e6ac40b39
He Linming <hlm52pk@163.com>
2022-08-02 13:33:39 +0800

    :bug: language should in a scope

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import { CONFIG } from "config";
+import { CONFIG, LANGUAGES } from "config";
 
 i18n
   // load translation using http -> see /public/locales
@@ -17,6 +17,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: CONFIG.defaultLanguate,
+    supportedLngs: LANGUAGES,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
