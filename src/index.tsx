6f92445606f1a233c0c4a59ed72d83ae2d556d0d
He Linming <hlm52pk@163.com>
2022-08-02 15:37:40 +0800

    :bookmark: release 0.2.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ColorModeScript } from "@chakra-ui/react";
 
@@ -10,10 +11,10 @@ import "./i18n";
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
-  <>
+  <StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
-  </>
+  </StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
