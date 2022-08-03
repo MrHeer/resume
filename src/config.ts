39ba10bedfd64cbe24b360f541f011975fa7818f
He Linming <hlm52pk@163.com>
2022-08-03 18:20:04 +0800

    :recycle: use config.json

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import config from "config.json";
+
 export const LANGUAGES = ["en_US", "zh_CN"] as const;
 
 export type Language = typeof LANGUAGES[number];
@@ -18,27 +20,7 @@ export type Config = {
   github: string;
 };
 
-export const CONFIG: Config = {
-  defaultLanguate: "zh_CN",
-  languages: [
-    {
-      resumeUrl: "/resume/resume_zh_CN.md",
-      icon: "🇨🇳",
-      description: "简体中文",
-      language: "zh_CN",
-    },
-    {
-      resumeUrl: "/resume/resume_en_US.md",
-      icon: "🇺🇸",
-      description: "English",
-      language: "en_US",
-    },
-  ],
-  mobile: "(+86)17623030229",
-  email: "hlm52pk@163.com",
-  twitter: "MrHeer5",
-  github: "MrHeer",
-};
+export const CONFIG: Config = config as Config;
 
 export const LANGUAGE_MAP = CONFIG.languages.reduce(
   (map, option) => map.set(option.language, option),
