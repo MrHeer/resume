c74346a39249561571c4c62124fcf4a3db52c427
He Linming <hlm52pk@163.com>
2022-08-04 17:20:19 +0800

    :rocket: service worker

--- src/index.tsx
+++ src/index.tsx
@@ -5,24 +5,31 @@ import { initConfig } from "config";
 import { theme } from "theme";
 import { initI18n } from "i18n";
 import App from "App";
-import * as serviceWorker from "serviceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 async function startApp() {
   const config = await initConfig();
   window.CONFIG = config;
   initI18n(config);
-  const container = document.getElementById("root");
-  const root = createRoot(container!);
+  const container = document.getElementById("root") as HTMLElement;
+  const root = createRoot(container);
   root.render(
     <StrictMode>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </StrictMode>
   );
-}
-startApp();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://cra.link/PWA
+  serviceWorkerRegistration.register();
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals(console.log);
+}
+
+startApp();
