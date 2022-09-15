f1c8f1257d78e3ce11303a68d593dd3a7aa9c511
He Linming <hlm52pk@163.com>
2022-09-15 18:18:15 +0800

    :lipstick: custom heading sizes

--- src/theme.ts
+++ src/theme.ts
@@ -30,4 +30,44 @@ const Menu: ComponentStyleConfig = {
   },
 };
 
-export const theme = extendTheme({ config, components: { Button, Menu } });
+const Heading: ComponentStyleConfig = {
+  sizes: {
+    "4xl": {
+      fontSize: "4xl",
+      lineHeight: 1,
+    },
+    "3xl": {
+      fontSize: "3xl",
+      lineHeight: 1,
+    },
+    "2xl": {
+      fontSize: "2xl",
+      lineHeight: [1.2, null, 1],
+    },
+    xl: {
+      fontSize: "xl",
+      lineHeight: [1.33, null, 1.2],
+    },
+    lg: {
+      fontSize: "lg",
+      lineHeight: [1.33, null, 1.2],
+    },
+    md: {
+      fontSize: "md",
+      lineHeight: 1.2,
+    },
+    sm: {
+      fontSize: "sm",
+      lineHeight: 1.2,
+    },
+    xs: {
+      fontSize: "xs",
+      lineHeight: 1.2,
+    },
+  },
+};
+
+export const theme = extendTheme({
+  config,
+  components: { Button, Menu, Heading },
+});
