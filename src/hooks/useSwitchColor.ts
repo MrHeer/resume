30570b9d51037cccd73ec3743dca739b17fff5d7
He Linming <hlm52pk@163.com>
2023-09-22 15:30:34 +0800

    :recycle: clean code

--- src/hooks/useSwitchColor.ts
+++ src/hooks/useSwitchColor.ts
@@ -12,27 +12,22 @@ export const useSwitchColor = () => {
   const { toggleColorMode } = useColorMode();
   return useCallback(
     (event: MouseEvent) => {
-      const skipTransition =
-        !document.startViewTransition ||
-        window.matchMedia("(prefers-reduced-motion: reduce)").matches;
-
-      const x = event.clientX;
-      const y = event.clientY;
-      const endRadius = Math.hypot(
-        Math.max(x, innerWidth - x),
-        Math.max(y, innerHeight - y)
-      );
+      const skipTransition = window.matchMedia(
+        "(prefers-reduced-motion: reduce)"
+      ).matches;
 
       const transition = transitionHelper({
         skipTransition,
-        updateDOM() {
-          flushSync(() => {
-            toggleColorMode();
-          });
-        },
+        updateDOM: () => flushSync(toggleColorMode),
       });
 
       transition.ready.then(() => {
+        const x = event.clientX;
+        const y = event.clientY;
+        const endRadius = Math.hypot(
+          Math.max(x, innerWidth - x),
+          Math.max(y, innerHeight - y)
+        );
         const clipPath = [
           `circle(0px at ${x}px ${y}px)`,
           `circle(${endRadius}px at ${x}px ${y}px)`,
