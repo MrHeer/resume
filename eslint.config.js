57d0922ac2c8d37a2c568ff58ad20b57d8015306
He Linming <hlm52pk@163.com>
2026-06-23 11:39:38 +0800

    chore: ignore .content-collections and src/components/ui in eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,11 @@ export default [
     },
   },
   {
-    ignores: ["eslint.config.js", ".prettierrc"],
+    ignores: [
+      "eslint.config.js",
+      ".prettierrc",
+      ".content-collections",
+      "src/components/ui",
+    ],
   },
 ]
