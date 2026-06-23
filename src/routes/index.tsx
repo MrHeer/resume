95dc25619c9c1bbe5a1da26742fb93585f543027
He Linming <hlm52pk@163.com>
2026-06-23 13:04:56 +0800

    chore: move locale redirect from `loader` to `beforeLoad`

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { fallbackSlug } from "@/lib/config"
 import { createFileRoute, redirect } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
-  loader: () => {
+  beforeLoad: () => {
     throw redirect({ to: "/$slug", params: { slug: fallbackSlug } })
   },
 })
